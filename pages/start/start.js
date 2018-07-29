6d7ad000c6446711e90f3f8e285e8dd1cf8054f2
super456 <1061996681@qq.com>
2018-07-29 12:20:13 +0800

    添加一个小程序的欢迎页面demo

---
+++ pages/start/start.js
@@ -0,0 +1,60 @@
+//login.js
+//获取应用实例
+var app = getApp();
+Page({
+  data: {
+    remind: '加载中',
+    angle: 0,
+    userInfo: {}
+  },
+  goToIndex: function() {
+    wx.switchTab({
+      url: '/pages/index/index',
+    });
+  },
+  onLoad: function() {
+    var that = this
+    wx.setNavigationBarTitle({
+      title: wx.getStorageSync('mallName')
+    })
+  },
+  onShow: function() {
+    let that = this
+    let userInfo = wx.getStorageSync('userInfo')
+    if (!userInfo) {
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+          })
+        }
+      })
+    } else {
+      that.setData({
+        userInfo: userInfo
+      })
+    }
+  },
+  onReady: function() {
+    var that = this;
+    setTimeout(function() {
+      that.setData({
+        remind: ''
+      });
+    }, 1000);
+    wx.onAccelerometerChange(function(res) {
+      var angle = -(res.x * 30).toFixed(1);
+      if (angle > 14) {
+        angle = 14;
+      } else if (angle < -14) {
+        angle = -14;
+      }
+      if (that.data.angle !== angle) {
+        that.setData({
+          angle: angle
+        });
+      }
+    });
+  }
+});
